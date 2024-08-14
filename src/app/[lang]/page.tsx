137e1dd2f775ff3ed0d95b887b11c8526a2356fb
Oscar Trollhag <oscar.trollhag@proton.me>
2024-08-14 10:47:59 +0200

    fix: improved print layout

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -105,76 +105,84 @@ export default async function Page({ params }: { params: { lang: keyof typeof la
             ))}
           </div>
         </div>
-        <div className="md:col-span-2">
-          <h3 className="text-2xl font-bold uppercase pl-8 pb-1 mb-2 border-b-2 border-emerald-600">
-            {lang.skills.title}
-          </h3>
-          <ul className="mx-3 md:mx-8">
-            {lang.skills.items.map((skill) => (
-              <li
-                key={skill}
-                className="inline-block m-1 px-2 bg-emerald-600 print-bg text-white rounded"
-              >
-                {skill}
-              </li>
-            ))}
-            <li className="inline-block m-1 px-2 rounded">{lang.skills.expand}</li>
-          </ul>
-          <h3 className="text-2xl font-bold uppercase pl-8 pb-1 mt-4 mb-2 border-b-2 border-emerald-600">
-            {lang.skills.languages.title}
-          </h3>
-          <ul className="mx-3 md:mx-8">
-            {Object.entries(lang.skills.languages.items).map(([language, value]) => (
-              <li key={language} className="flex my-2">
-                <span className="block w-24">{language}</span>
-                <span className="flex">
-                  {Array(5)
-                    .fill(undefined)
-                    .map((_, i) => (
-                      <span
-                        key={i}
-                        className={`block m-1 w-4 h-4 border-2 rounded border-emerald-600 ${i + 1 <= value ? 'bg-emerald-600 print-bg' : ''}`}
-                      />
-                    ))}
-                </span>
-              </li>
-            ))}
-          </ul>
-          <h3 className="text-2xl font-bold uppercase pl-8 pb-1 mt-4 mb-2 border-b-2 border-emerald-600">
-            {lang.certificates.title}
-          </h3>
-          <ul className="mx-3 md:mx-8">
-            {lang.certificates.items.map((cert, i) => (
-              <li key={i} className="my-2">
-                {cert.title} {cert.date && `(${cert.date})`}
-              </li>
-            ))}
-          </ul>
-
-          <h3 className="text-2xl font-bold uppercase pl-8 pb-1 mt-4 mb-2 border-b-2 border-emerald-600">
-            {lang.education.title}
-          </h3>
-          <div className="mx-3 md:mx-8">
-            {lang.education.items.map((item, i) => (
-              <div key={i}>
-                <h4 className="text-lg">
-                  <b>{item.title}</b>
-                  <br />
-                  {item.institution}
-                </h4>
-                <div className="flex justify-between my-2">
-                  <span className="bg-emerald-600 bg-opacity-15 px-2 rounded text-emerald-700 print-bg">
-                    {item.date.join(' - ')}
-                  </span>
-                </div>
-              </div>
-            ))}
+        <div className="md:col-span-2 print:grid gap-4 print:grid-cols-5">
+          <div className="print:col-span-3 mb-4">
+            <h3 className="text-2xl font-bold uppercase pl-8 pb-1 mb-2 border-b-2 border-emerald-600">
+              {lang.skills.title}
+            </h3>
+            <ul className="mx-3 md:mx-8">
+              {lang.skills.items.map((skill) => (
+                <li
+                  key={skill}
+                  className="inline-block m-1 px-2 bg-emerald-600 print-bg text-white rounded"
+                >
+                  {skill}
+                </li>
+              ))}
+              <li className="inline-block m-1 px-2 rounded">{lang.skills.expand}</li>
+            </ul>
           </div>
-
-          <h3 className="text-2xl font-bold uppercase pl-8 pb-1 mt-4 mb-2 border-b-2 border-emerald-600">
-            {lang.general.references.title}
-          </h3>
-          <p className="mx-3 md:mx-8">{lang.general.references.description}</p>
+          <div className="print:col-span-2 mb-4">
+            <h3 className="text-2xl font-bold uppercase pl-8 pb-1 mb-2 border-b-2 border-emerald-600">
+              {lang.skills.languages.title}
+            </h3>
+            <ul className="mx-3 md:mx-8">
+              {Object.entries(lang.skills.languages.items).map(([language, value]) => (
+                <li key={language} className="flex my-2">
+                  <span className="block w-24">{language}</span>
+                  <span className="flex">
+                    {Array(5)
+                      .fill(undefined)
+                      .map((_, i) => (
+                        <span
+                          key={i}
+                          className={`block m-1 w-4 h-4 border-2 rounded border-emerald-600 ${i + 1 <= value ? 'bg-emerald-600 print-bg' : ''}`}
+                        />
+                      ))}
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="print:col-span-3 mb-4">
+            <h3 className="text-2xl font-bold uppercase pl-8 pb-1 mb-2 border-b-2 border-emerald-600">
+              {lang.certificates.title}
+            </h3>
+            <ul className="mx-3 md:mx-8">
+              {lang.certificates.items.map((cert, i) => (
+                <li key={i} className="my-2">
+                  {cert.title} {cert.date && `(${cert.date})`}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="print:col-span-2 mb-4">
+            <h3 className="text-2xl font-bold uppercase pl-8 pb-1 mb-2 border-b-2 border-emerald-600">
+              {lang.education.title}
+            </h3>
+            <div className="mx-3 md:mx-8">
+              {lang.education.items.map((item, i) => (
+                <div key={i}>
+                  <h4 className="text-lg">
+                    <b>{item.title}</b>
+                    <br />
+                    {item.institution}
+                  </h4>
+                  <div className="flex justify-between my-2">
+                    <span className="bg-emerald-600 bg-opacity-15 px-2 rounded text-emerald-700 print-bg">
+                      {item.date.join(' - ')}
+                    </span>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="print:col-span-3">
+            <h3 className="text-2xl font-bold uppercase pl-8 pb-1 mb-2 border-b-2 border-emerald-600">
+              {lang.general.references.title}
+            </h3>
+            <p className="mx-3 md:mx-8">{lang.general.references.description}</p>
+          </div>
         </div>
       </div>
       <LinkTree className="pt-8 md:hidden print:hidden" items={headerList} />
