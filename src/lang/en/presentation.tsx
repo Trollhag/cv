e85e92d9218eb9681acd367c9e78eea6b2770441
Oscar Trollhag <oscar.trollhag@proton.me>
2024-08-15 21:09:06 +0200

    fix: updated role presentation

--- src/lang/en/presentation.tsx
+++ src/lang/en/presentation.tsx
@@ -1,9 +1,8 @@
 export const presentation = {
   role: [
-    'I am a Full Stack Developer with a strong expertise in JavaScript, particularly with React, Node.js, and TypeScript. My deep understanding of JavaScript allows me to build robust and efficient applications across both front-end and back-end environments.',
-    'Excelling in architectural design, I ensuring that systems are scalable, maintainable, and built with best practices in mind. Dedicated to maintaining high-quality standards, I like writing automated tests and prefer Test-Driven Development practices where applicable to ensure quality.',
+    'My expertise is primarily in the JavaScript ecosystem, with a particular focus on React, Node.js, and TypeScript. I have extensive experience in developing dynamic and user-friendly interfaces with React, as well as building scalable and efficient backend solutions with Node.js. With a deep understanding of TypeScript, I can create robust and maintainable code that minimizes the risk of bugs and improves the development flow. I am also currently learning GoLang and look forward to deepening my knowledge of the language to expand my skills and take on new types of challenges in the future.',
+    'Dedicated to maintaining high quality, I like to write automated tests and prefer Test-Driven Development principles where appropriate to ensure quality.',
     'Thriving in collaborative environments, I enjoy peer-reviewing code. I am skilled at finding improvements and providing constructive feedback, which helps foster a culture of continuous improvement and learning within the team.',
-    'I aspire to become a Tech Lead and I take on some leadership responsibilities, mentor junior developers, and drive the technical direction of projects.',
   ],
   personal: [
     'As a person, I am very exploratory and meticulous, I always try to find new and better ways to do things, understanding both the whole concept and details in everything I do. I am active in meetings, and try to include everyone, so opinions and thoughts are shared from all participants.',
--- src/lang/sv/presentation.tsx
+++ src/lang/sv/presentation.tsx
@@ -1,9 +1,8 @@
 export const presentation = {
   role: [
-    'Jag är en Fullstack-utvecklare med expertis inom JavaScript, särskilt med React, Node.js och TypeScript. Min djupa förståelse för JavaScript gör att jag kan bygga effektiva applikationer både på front-end och back-end.',
-    'Jag utmärker mig inom system design och ser till att systemen är skalbara, går att underhålla och byggda med bästa praxis i åtanke. Dedikerad till att upprätthålla hög kvalitet, gillar jag att skriva automatiserade tester och föredrar Test-Driven Development-principer där det är lämpligt för att säkerställa kvaliteten.',
+    'Min expertis ligger främst inom JavaScript-ekosystemet, med särskilt fokus på React, Node.js och TypeScript. Jag har omfattande erfarenhet av att utveckla dynamiska och användarvänliga gränssnitt med React, samt att bygga skalbara och effektiva backend-lösningar med Node.js. Med en djup förståelse för TypeScript kan jag skapa robust och lättunderhållen kod som minimerar risken för buggar och förbättrar utvecklingsflödet. Just nu håller jag även på att lära mig GoLang och ser fram emot att fördjupa mig i språket för att kunna bredda min kompetens och ta mig an nya typer av utmaningar i framtiden.',
+    'Dedikerad till att upprätthålla hög kvalitet, gillar jag att skriva automatiserade tester och föredrar Test-Driven Development-principer där det är lämpligt för att säkerställa kvaliteten.',
     'Jag trivs i samarbetsmiljöer och tycker om att granska kod och parprogrammera med kollegor. Jag är skicklig på att hitta förbättringar och ge konstruktiv feedback, vilket hjälper till att främja en kultur av kontinuerlig förbättring och lärande inom teamet.',
-    'På sikt vill jag bli en Tech Lead och ta på mig ledaransvar, handleda juniora utvecklare och driva den tekniska riktningen för projekt.',
   ],
   personal: [
     'Som person är jag väldigt utforskande och noggrann, jag försöker alltid hitta nya och bättre sätt att göra saker på, förstå både hela koncept och detaljer i allt jag gör. På möten tar jag gärna plats, och försöker då inkludera alla, så åsikter och tankar delas från alla deltagare.',
