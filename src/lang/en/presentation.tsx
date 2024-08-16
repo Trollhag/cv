1b7ee2ff3a4895abc3375531b5884007139fbfd9
Trollhag <oscar@trollhag.com>
2024-08-16 14:31:19 +0200

    fix: minor updates to role presentation

--- src/lang/en/presentation.tsx
+++ src/lang/en/presentation.tsx
@@ -2,7 +2,7 @@ export const presentation = {
   role: [
     'My expertise is primarily in the JavaScript ecosystem, with a particular focus on React, Node.js, and TypeScript. I have extensive experience in developing dynamic and user-friendly interfaces with React, as well as building scalable and efficient backend solutions with Node.js. With a deep understanding of TypeScript, I can create robust and maintainable code that minimizes the risk of bugs and improves the development flow. I am also currently learning GoLang and look forward to deepening my knowledge of the language to expand my skills and take on new types of challenges in the future.',
     'Dedicated to maintaining high quality, I like to write automated tests and prefer Test-Driven Development principles where appropriate to ensure quality.',
-    'Thriving in collaborative environments, I enjoy peer-reviewing code. I am skilled at finding improvements and providing constructive feedback, which helps foster a culture of continuous improvement and learning within the team.',
+    'Thriving in collaborative environments, I enjoy peer-reviewing code. I am skilled at finding improvements and providing constructive feedback, which helps foster a culture of continuous improvement and learning within the team. I am used to working closely with product owners and stakeholders to gain an understanding of requirements and be able to suggest improvements.',
   ],
   personal: [
     'As a person, I am very exploratory and meticulous, I always try to find new and better ways to do things, understanding both the whole concept and details in everything I do. I am active in meetings, and try to include everyone, so opinions and thoughts are shared from all participants.',
--- src/lang/sv/presentation.tsx
+++ src/lang/sv/presentation.tsx
@@ -1,8 +1,8 @@
 export const presentation = {
   role: [
     'Min expertis ligger främst inom JavaScript-ekosystemet, med särskilt fokus på React, Node.js och TypeScript. Jag har omfattande erfarenhet av att utveckla dynamiska och användarvänliga gränssnitt med React, samt att bygga skalbara och effektiva backend-lösningar med Node.js. Med en djup förståelse för TypeScript kan jag skapa robust och lättunderhållen kod som minimerar risken för buggar och förbättrar utvecklingsflödet. Just nu håller jag även på att lära mig GoLang och ser fram emot att fördjupa mig i språket för att kunna bredda min kompetens och ta mig an nya typer av utmaningar i framtiden.',
-    'Dedikerad till att upprätthålla hög kvalitet, gillar jag att skriva automatiserade tester och föredrar Test-Driven Development-principer där det är lämpligt för att säkerställa kvaliteten.',
-    'Jag trivs i samarbetsmiljöer och tycker om att granska kod och parprogrammera med kollegor. Jag är skicklig på att hitta förbättringar och ge konstruktiv feedback, vilket hjälper till att främja en kultur av kontinuerlig förbättring och lärande inom teamet.',
+    'Dedikerad till att upprätthålla hög kvalitet gillar jag att skriva automatiserade tester och föredrar Test-Driven Development-principer där det är lämpligt för att säkerställa kvaliteten.',
+    'Jag trivs i samarbetsmiljöer och tycker om att granska kod och parprogrammera med kollegor. Jag är skicklig på att hitta förbättringar och ge konstruktiv feedback, vilket hjälper till att främja en kultur av kontinuerlig förbättring och lärande inom teamet. Jag är van att jobba nära produktägare och stakeholders för att få forståelse för krav och kunna föreslå förbättningar.',
   ],
   personal: [
     'Som person är jag väldigt utforskande och noggrann, jag försöker alltid hitta nya och bättre sätt att göra saker på, förstå både hela koncept och detaljer i allt jag gör. På möten tar jag gärna plats, och försöker då inkludera alla, så åsikter och tankar delas från alla deltagare.',
