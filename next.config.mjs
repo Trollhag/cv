f70c1747c42d285068a8ea2a4b8bfa6a29debdff
Oscar Trollhag <oscar@trollhag.com>
2025-04-02 19:29:02 +0200

    fix: svg import issue after upgrade

--- next.config.mjs
+++ next.config.mjs
@@ -3,19 +3,17 @@ const nextConfig = {
   basePath: process.env.NEXT_BASE_PATH || '',
   output: 'export',
   trailingSlash: true,
-  webpack: function (config, context) {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    }
-
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    })
-    return config
-  },
   distDir: 'build',
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
+  },
 }
 
 export default nextConfig
